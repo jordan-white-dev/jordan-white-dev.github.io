c8777b85f34e5b334ac158ec375e1ab4567b62bf
Jordan <jlwhite1991@gmail.com>
2026-02-19 16:39:12 -0500

    chore: refactored inputmodes to have reusable components

--- src/lib/pages/home/components/inputmodes.tsx
+++ src/lib/pages/home/components/inputmodes.tsx
@@ -1,34 +1,50 @@
 import { Icon, RadioCard, SimpleGrid } from "@chakra-ui/react";
+import type { ReactNode } from "react";
 
 import { CenterSVG, ColorSVG, CornerSVG, DigitSVG } from "./svgs";
 import { Tooltip } from "./tooltip";
 
-const inputs = [
-  {
-    value: "digit",
-    label: "Digit",
-    icon: <DigitSVG />,
-    tooltip: "Digit input mode",
-  },
-  {
-    value: "color",
-    label: "Color",
-    icon: <ColorSVG />,
-    tooltip: "Color markup mode",
-  },
-  {
-    value: "corner",
-    label: "Corner",
-    icon: <CornerSVG />,
-    tooltip: "Corner markup mode",
-  },
-  {
-    value: "center",
-    label: "Center",
-    icon: <CenterSVG />,
-    tooltip: "Center markup mode",
-  },
-];
+type InputModeItemProps = {
+  ariaLabel: string;
+  icon: ReactNode;
+  inputModeValue: string;
+  tooltipText: string;
+};
+
+export const InputModeItem = ({
+  ariaLabel,
+  icon,
+  inputModeValue,
+  tooltipText,
+  ...props
+}: InputModeItemProps) => {
+  return (
+    <RadioCard.Item
+      alignItems="center"
+      aria-label={ariaLabel}
+      padding="0"
+      value={inputModeValue}
+      {...props}
+    >
+      <RadioCard.ItemHiddenInput />
+      <Tooltip content={tooltipText} positioning={{ placement: "right-start" }}>
+        <RadioCard.ItemControl padding="0">
+          <Icon
+            boxSize={{
+              base: "1.922rem",
+              sm: "2.625rem",
+              md: "3.72rem",
+              lg: 20,
+            }}
+            fill="black"
+          >
+            {icon}
+          </Icon>
+        </RadioCard.ItemControl>
+      </Tooltip>
+    </RadioCard.Item>
+  );
+};
 
 export const InputModes = () => (
   <RadioCard.Root
@@ -42,35 +58,30 @@ export const InputModes = () => (
       gap={{ base: "0.229rem", sm: "1", md: "0.5833rem", lg: "3" }}
       minWidth={{ lg: "12.75rem" }}
     >
-      {inputs.map((input) => (
-        <RadioCard.Item
-          alignItems="center"
-          aria-label={input.label}
-          key={input.value}
-          padding="0"
-          value={input.value}
-        >
-          <RadioCard.ItemHiddenInput />
-          <Tooltip
-            content={input.tooltip}
-            positioning={{ placement: "right-start" }}
-          >
-            <RadioCard.ItemControl padding="0">
-              <Icon
-                boxSize={{
-                  base: "1.922rem",
-                  sm: "2.625rem",
-                  md: "3.72rem",
-                  lg: 20,
-                }}
-                fill="black"
-              >
-                {input.icon}
-              </Icon>
-            </RadioCard.ItemControl>
-          </Tooltip>
-        </RadioCard.Item>
-      ))}
+      <InputModeItem
+        ariaLabel="Digit"
+        icon={<DigitSVG />}
+        inputModeValue="digit"
+        tooltipText="Digit input mode"
+      />
+      <InputModeItem
+        ariaLabel="Color"
+        icon={<ColorSVG />}
+        inputModeValue="color"
+        tooltipText="Color markup mode"
+      />
+      <InputModeItem
+        ariaLabel="Center"
+        icon={<CenterSVG />}
+        inputModeValue="center"
+        tooltipText="Center markup mode"
+      />
+      <InputModeItem
+        ariaLabel="Corner"
+        icon={<CornerSVG />}
+        inputModeValue="corner"
+        tooltipText="Corner markup mode"
+      />
     </SimpleGrid>
   </RadioCard.Root>
 );

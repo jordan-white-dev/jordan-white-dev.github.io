cbd9ece546645dae87fb678aa4fa96b594e76a46
Jordan <jlwhite1991@gmail.com>
2026-02-19 17:06:29 -0500

    chore: refactored inputpad to have reusable components

--- src/lib/pages/home/components/inputpad.tsx
+++ src/lib/pages/home/components/inputpad.tsx
@@ -1,5 +1,5 @@
 import {
-  // ColorSwatch,
+  ColorSwatch,
   GridItem,
   Icon,
   IconButton,
@@ -16,11 +16,11 @@ import { GrCheckbox, GrMultiple } from "react-icons/gr";
 
 import { Tooltip } from "./tooltip";
 
-// const COLOR_SWATCH_SIZE: IconProps["width"] = {
-//   base: "2.105rem",
-//   sm: "11",
-//   md: "16",
-// };
+const COLOR_SWATCH_SIZE: IconProps["width"] = {
+  base: "2.105rem",
+  sm: "11",
+  md: "16",
+};
 const ICON_SIZE: IconProps["width"] = { base: "6", sm: "8", md: "11" };
 const ICON_BUTTON_SIZE: IconButtonProps["size"] = {
   base: "xs",
@@ -33,20 +33,42 @@ const ICON_BUTTON_TEXT_STYLE: IconButtonProps["textStyle"] = {
   md: "5xl",
 };
 
-// const ColorInput = (buttonColor: string) => {
-//   return (
-//     <GridItem colSpan={2} height={COLOR_SWATCH_SIZE} width={COLOR_SWATCH_SIZE}>
-//       <ColorSwatch
-//         height={COLOR_SWATCH_SIZE}
-//         rounded="md"
-//         value={buttonColor}
-//         width={COLOR_SWATCH_SIZE}
-//       />
-//     </GridItem>
-//   );
-// };
+type ColorInputsProps = {
+  buttonColor: string;
+};
 
-const NumberInput = (buttonValue: string) => {
+const ColorInput = ({ buttonColor }: ColorInputsProps) => {
+  return (
+    <GridItem colSpan={2} height={COLOR_SWATCH_SIZE} width={COLOR_SWATCH_SIZE}>
+      <ColorSwatch
+        height={COLOR_SWATCH_SIZE}
+        rounded="md"
+        value={buttonColor}
+        width={COLOR_SWATCH_SIZE}
+      />
+    </GridItem>
+  );
+};
+
+const ColorPadInputs = () => (
+  <>
+    <ColorInput buttonColor="#666666" />
+    <ColorInput buttonColor="#b0b0b0" />
+    <ColorInput buttonColor="#ffffff" />
+    <ColorInput buttonColor="#f690f6" />
+    <ColorInput buttonColor="#f98987" />
+    <ColorInput buttonColor="#c69c78" />
+    <ColorInput buttonColor="#efc084" />
+    <ColorInput buttonColor="#ffff75" />
+    <ColorInput buttonColor="#d1efa6" />
+  </>
+);
+
+type NumberInputProps = {
+  buttonValue: string;
+};
+
+const NumberInput = ({ buttonValue }: NumberInputProps) => {
   return (
     <GridItem colSpan={2}>
       <Square aspectRatio="square">
@@ -65,83 +87,93 @@ const NumberInput = (buttonValue: string) => {
   );
 };
 
-export const InputPad = () => (
-  <SimpleGrid
-    columns={6}
-    gap={{ base: "0.1874rem", sm: "1", md: "1.5" }}
-    height="fit-content"
-  >
-    {NumberInput("1")}
-    {NumberInput("2")}
-    {NumberInput("3")}
-    {NumberInput("4")}
-    {NumberInput("5")}
-    {NumberInput("6")}
-    {NumberInput("7")}
-    {NumberInput("8")}
-    {NumberInput("9")}
-    {/* {ColorInput("#666666")}
-    {ColorInput("#b0b0b0")}
-    {ColorInput("#ffffff")}
-    {ColorInput("#f690f6")}
-    {ColorInput("#f98987")}
-    {ColorInput("#c69c78")}
-    {ColorInput("#efc084")}
-    {ColorInput("#ffff75")}
-    {ColorInput("#d1efa6")} */}
-    <GridItem
-      alignContent="center"
-      border={{ sm: "2px solid" }}
-      borderColor={{ sm: "blue.border" }}
-      colSpan={3}
-      height="full"
-      rounded="md"
-      width="full"
-    >
-      <Tooltip
-        content="Multiple cells can be selected while this is toggled"
-        key="multiselect-tooltip"
-      >
-        <Stack alignItems="center" direction="column" gap="1">
-          <Switch.Root colorPalette="blue" size="lg">
-            <Switch.HiddenInput />
-            <Switch.Control>
-              <Switch.Thumb />
-              <Switch.Indicator fallback={<Icon as={GrCheckbox} />}>
-                <Icon as={GrMultiple} />
-              </Switch.Indicator>
-            </Switch.Control>
-          </Switch.Root>
-          <Text
-            alignSelf="center"
-            fontWeight="semibold"
-            hideBelow="md"
-            justifySelf="center"
-          >
-            Multiselect
-          </Text>
-        </Stack>
-      </Tooltip>
-    </GridItem>
-    <GridItem colSpan={3}>
-      <Tooltip
-        content="Delete content from the selected cells"
-        positioning={{ placement: "bottom" }}
-      >
-        <IconButton
-          aria-label="Delete"
-          color="white"
-          colorPalette="blue"
-          rounded="md"
-          size={ICON_BUTTON_SIZE}
-          textStyle={ICON_BUTTON_TEXT_STYLE}
-          width="full"
-        >
-          <Icon height={ICON_SIZE} width={ICON_SIZE}>
-            <FiDelete />
-          </Icon>
-        </IconButton>
-      </Tooltip>
-    </GridItem>
-  </SimpleGrid>
+const NumberPadInputs = () => (
+  <>
+    <NumberInput buttonValue="1" />
+    <NumberInput buttonValue="2" />
+    <NumberInput buttonValue="3" />
+    <NumberInput buttonValue="4" />
+    <NumberInput buttonValue="5" />
+    <NumberInput buttonValue="6" />
+    <NumberInput buttonValue="7" />
+    <NumberInput buttonValue="8" />
+    <NumberInput buttonValue="9" />
+  </>
 );
+
+const MultiselectSwitch = () => (
+  <GridItem
+    alignContent="center"
+    border={{ sm: "2px solid" }}
+    borderColor={{ sm: "blue.border" }}
+    colSpan={3}
+    height="full"
+    rounded="md"
+    width="full"
+  >
+    <Tooltip
+      content="Multiple cells can be selected while this is toggled"
+      key="multiselect-tooltip"
+    >
+      <Stack alignItems="center" direction="column" gap="1">
+        <Switch.Root colorPalette="blue" size="lg">
+          <Switch.HiddenInput />
+          <Switch.Control>
+            <Switch.Thumb />
+            <Switch.Indicator fallback={<Icon as={GrCheckbox} />}>
+              <Icon as={GrMultiple} />
+            </Switch.Indicator>
+          </Switch.Control>
+        </Switch.Root>
+        <Text
+          alignSelf="center"
+          fontWeight="semibold"
+          hideBelow="md"
+          justifySelf="center"
+        >
+          Multiselect
+        </Text>
+      </Stack>
+    </Tooltip>
+  </GridItem>
+);
+
+const DeleteButton = () => (
+  <GridItem colSpan={3}>
+    <Tooltip
+      content="Delete content from the selected cells"
+      positioning={{ placement: "bottom" }}
+    >
+      <IconButton
+        aria-label="Delete"
+        color="white"
+        colorPalette="blue"
+        rounded="md"
+        size={ICON_BUTTON_SIZE}
+        textStyle={ICON_BUTTON_TEXT_STYLE}
+        width="full"
+      >
+        <Icon height={ICON_SIZE} width={ICON_SIZE}>
+          <FiDelete />
+        </Icon>
+      </IconButton>
+    </Tooltip>
+  </GridItem>
+);
+
+export const InputPad = () => {
+  const isNumberMode = true; // Will eventually be hooked up to state
+
+  return (
+    <SimpleGrid
+      columns={6}
+      gap={{ base: "0.1874rem", sm: "1", md: "1.5" }}
+      height="fit-content"
+    >
+      {isNumberMode ? <NumberPadInputs /> : <ColorPadInputs />}
+
+      <MultiselectSwitch />
+      <DeleteButton />
+    </SimpleGrid>
+  );
+};

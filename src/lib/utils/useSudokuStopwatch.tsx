c973304fda1a1b16646945e5792cefaafa6706f3
Jordan <jlwhite1991@gmail.com>
2026-03-15 17:15:49 -0400

    fix: correct handling of minutes 60-99 on stopwatch

--- src/lib/utils/useSudokuStopwatch.tsx
+++ src/lib/utils/useSudokuStopwatch.tsx
@@ -24,6 +24,10 @@ const getFormattedStopwatchMinutes = (
     const totalMinutes = minutes + hoursConvertedToMinutes;
 
     return String(totalMinutes).padStart(3, "0");
+  } else if (hours === 1) {
+    const totalMinutes = minutes + 60;
+
+    return String(totalMinutes).padStart(2, "0");
   }
 
   return String(minutes).padStart(2, "0");

605e6a059faf0904d95507750a8eaa9b1dab100e
Jordan <jlwhite1991@gmail.com>
2026-03-18 13:21:43 -0400

    refactor: moved vitest configs into vite.config and deleted old file; added react compiler and removed unused configs

--- vite.config.ts
+++ vite.config.ts
@@ -1,43 +1,24 @@
-/* eslint-disable import/no-extraneous-dependencies */
+/// <reference types="vitest/config" />
 
+import babel from "@rolldown/plugin-babel";
 import { devtools } from "@tanstack/devtools-vite";
 import { tanstackRouter } from "@tanstack/router-plugin/vite";
-import { visualizer } from "rollup-plugin-visualizer";
-import type { PluginOption } from "vite";
+import react, { reactCompilerPreset } from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 import checker from "vite-plugin-checker";
-import type { VitePWAOptions } from "vite-plugin-pwa";
-import { VitePWA } from "vite-plugin-pwa";
 import tsconfigPaths from "vite-tsconfig-paths";
 
-const pwaOptions: Partial<VitePWAOptions> = {
-  // TODO: enable if you want to enable PWA service worker
-  disable: true,
-  registerType: "autoUpdate",
-  manifest: {
-    short_name: "sudoku",
-    name: "Sudoku",
-    lang: "en",
-    start_url: "/",
-    background_color: "#FFFFFF",
-    theme_color: "#FFFFFF",
-    dir: "ltr",
-    display: "standalone",
-    prefer_related_applications: false,
-  },
-  pwaAssets: {
-    disabled: false,
-    config: true,
-  },
-};
-
-// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
   const isCheckDisabled = mode === "production" || !!process.env.VITEST;
+
   return {
     plugins: [
       devtools(),
+      babel({
+        presets: [reactCompilerPreset()],
+      }),
       tanstackRouter({ autoCodeSplitting: true }),
+      react(),
       ...(!isCheckDisabled
         ? [
             checker({
@@ -46,12 +27,16 @@ export default defineConfig(({ mode }) => {
           ]
         : []),
       tsconfigPaths(),
-      visualizer({ template: "sunburst" }) as unknown as PluginOption,
-      VitePWA(pwaOptions),
     ],
     server: {
       port: 3000,
       open: true,
+    },
+    test: {
+      coverage: {
+        provider: "v8",
+        include: ["src/lib/utils/**/*.{ts,tsx,js,jsx}"],
+      },
     },
   };
 });
